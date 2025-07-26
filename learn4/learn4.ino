20cc84045293af549e8465b92f14f54761f75d92
Syed Fardin <syedfardin0612@gmail.com>
2025-07-26 21:08:20 +0530

    Memory management question ongoing

---
+++ learn4/learn4.ino
@@ -0,0 +1,78 @@
+#if CONFIG_FREERTOS_UNICORE
+static const int app_cpu = 0;
+#else
+static const int app_cpu = 1;
+#endif
+
+char* ptr;int length = 0;
+int buff_size = 255;
+bool flag = false;
+
+
+void task1(void *param){
+  char ch;
+  char buff[buff_size];
+  int index = 0;
+
+  while(1){
+    if(Serial.available()){
+      ch = Serial.read();
+      if(ch=='\n'){
+        ptr = (char*)pvPortMalloc(sizeof(char)*(index+1));
+        for(int i=0;i<=index;i++){
+          ptr[i] = buff[i];
+        }
+        length = index+1;
+        flag = true;
+        index = 0;
+        continue;
+      }
+      buff[index] = ch;
+      index++; 
+    }
+  }
+}
+
+void task2(void *param){
+  int i;
+  while(1){
+    if(flag){
+      Serial.println("Flagged");
+      for(i=0;i<length;i++){
+        Serial.print(ptr[i]);
+      }
+      Serial.println("");
+      vPortFree(ptr);
+      flag = false;
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Serial port started");
+  xTaskCreatePinnedToCore(
+    task1,
+    "task1",
+    (buff_size*sizeof(char))+800,
+    NULL,
+    1,
+    NULL,
+    app_cpu
+  );
+    xTaskCreatePinnedToCore(
+    task2,
+    "task2",
+    1024,
+    NULL,
+    1,
+    NULL,
+    app_cpu
+  );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
