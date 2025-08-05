1f849a94fa1182840ba3b0d2d710fc10d7c7221f
Fardin <syedfardin0612@gmail.com>
2025-08-05 18:04:03 +0530

    Queue challange taskA done;

---
+++ queue_challange/queue_challange.ino
@@ -0,0 +1,83 @@
+#if CONFIG_FREE_RTOS_UNICORE
+static const int app_cpu = 0;
+#else
+static const int app_cpu = 1;
+#endif
+
+static const uint8_t max_queue_len = 5;
+
+static QueueHandle_t queue1;
+static QueueHandle_t queue2;
+
+static const uint8_t led_pin = NULL;
+
+static const int buff_size = NULL;
+
+String inputString = "";
+bool stringEnd = false;
+
+String* splitBySpace(String str){
+  static String words[10];int i=0;String word = "";int wordIndex = 0;
+  while(i<str.length()){
+    if((str[i]==' ')){
+      words[wordIndex] = word;
+      word="";
+      wordIndex++;i++;
+      continue;
+    }
+    if(i==str.length()-1){
+      word+=str[i];
+      words[wordIndex] = word;break;
+    }
+    word+=str[i];
+    i++;
+  }
+  return words;
+}
+
+void taskA(void *param){
+  char msg[10];char ch;String *commands;int msg;
+  while(1){
+    if(xQueueReceive(queue1,msg,0)==pdTRUE){
+      Serial.println(msg);
+    }
+
+    if(Serial.available()){
+      ch = Serial.read();\
+      inputString+=ch;
+      if(ch=='\n'){
+        inputString.trim();
+        stringEnd = true;
+      }
+    }
+    if(stringEnd){
+      Serial.println(inputString);
+      commands = splitBySpace(inputString);
+      inputSring = "";
+      stringEnd = false;
+
+      if(commands[0]=="delay"){
+        msg = commands[1].toInt();
+        if(xQueueSend(queue2,(void*)&msg,10)!=pdTRUE){
+          Serial.println("Queue2 Full!");
+        }
+      }
+    }
+
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("------------FREE RTOS Started--------------------");
+
+  queue1 = xQueueCreate(max_queue_len,sizeof(int));
+  queue2 = xQueueCreate(max_queue_len,sizeof(char)*10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
