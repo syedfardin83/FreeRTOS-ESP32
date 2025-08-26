62322334829e8d2d266e6dd86626aeb7b417f146
Fardin <syedfardin0612@gmail.com>
2025-08-26 19:14:40 +0530

    Spin Locks, diff btw portENTER_CRITICAL and taskENTER_CRITICAL; Code not Tested

--- hardware_interrupts/hardware_interrupts.ino
+++ hardware_interrupts/hardware_interrupts.ino
@@ -1,3 +1,9 @@
+#if CONFIG_FREE_RTOS_UNICORE
+static const int app_cpu = 0;
+#else
+static const int app_cpu = 1;
+#endif
+
 //Timer settings
 static uint16_t prescaler = 80;
 static unint64_t timer_max = 1000000;
@@ -5,18 +11,44 @@ static unint64_t timer_max = 1000000;
 //Timer handler
 static hw_timer_t *my_timer = NULL;
 
-//LED
-#define LED_PIN NULL
+static volatile int isr_counter = 0;
+
+//  Spin Lock Handler
+static portMUX_TYPE my_spinlock = portMUX_INITIALIZER_UNLOCKED;
+
 
 //ISR defined in the internal RAM (not in Flash memory) for faster access
 void IRAM_ATTR myTimerInterrupt(){
-  int curr_state = digitalRead(led_pin);
-  digitalWrite(led_pin, !curr_state);
+
+  portENTER_CRITICAL(&my_spinlock);
+
+  isr_counter++;
+
+  portEXIT_CRITICAL(&my_spinlock);
+
+}
+
+void printTask(void* params){
+  while(1){
+    Serial.println(isr_counter);
+
+    portENTER_CRITICAL(&my_spinlock);
+    isr_counter--;
+    portEXIT_CRITICAL(&my_spinlock);
+  }
 }
 
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  xTaskCreatePinnedToCore(
+    printTask,
+    "Print Task",
+    1024,
+    NULL,
+    1,
+    NULL,
+    app_cpu
+  );
 
   //  Create and start timer: (timer, prescaler, count up)
   my_timer = timerBegin(0, prescaler, true);
